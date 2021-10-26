309d432d8c0c7afcad67448ae210f4c6924493a5
Boris Chen <borischen0203@gmail.com>
2021-10-26 12:10:59 +0100

    UPD. update OnHTML function to get job id

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gocolly/colly"
@@ -31,7 +32,7 @@ func main() {
 	//Set up collector
 	collector := colly.NewCollector(
 		colly.AllowedDomains("uk.indeed.com"),
-		colly.Async(true),
+		// colly.Async(true),
 	)
 
 	// collector2 := collector.Clone()
@@ -47,39 +48,37 @@ func main() {
 
 	jobKey = make(map[string]string)
 
-	collector.OnHTML(".mosaic-provider-jobcards", func(e *colly.HTMLElement) {
+	collector.OnHTML("#mosaic-provider-jobcards", func(e *colly.HTMLElement) {
 
-		e.ForEach(".slider_container", func(_ int, el *colly.HTMLElement) {
+		e.ForEach(".tapItem", func(_ int, el *colly.HTMLElement) {
 			temp := job{
-				//TODO: optimize the goQuerySelector to get job id
-				// ID:      el.ChildAttr(".tapItem", "data-jk"),
+				ID:              el.Attr("data-jk"),
 				Title:           el.ChildAttr(".jobTitle-color-purple > span", "title"),
 				Company:         el.ChildText("span.companyName"),
-				CompanyLocation: el.ChildText("div.companyLocation"),
+				CompanyLocation: removeS(el.ChildText("div.heading6.company_location.tapItem-gutter > pre > div")),
 				Salary:          el.ChildText(".salary-snippet-container .salary-snippet > span"),
 			}
 
-			//TODO: Use job id as key to check the jobs duplicate or not
-			// if _, exist := jobKey[temp.ID]; !exist {
-			// 	count++
-			// 	jobKey[temp.ID] = ""
-			// 	allJobs = append(allJobs, temp)
-			// 	fmt.Println(temp.ID + "," + temp.Title + "," + temp.Company + "," + temp.CompanyLocation + "," + temp.Salary)
-			// }
-
-			allJobs = append(allJobs, temp)
-			// fmt.Println(temp.ID + "," + temp.Title + "," + temp.Company + "," + temp.CompanyLocation + "," + temp.Salary)
-			count++
+			//Use job id as key to check the crawler job is duplicate or not
+			if _, exist := jobKey[temp.ID]; !exist { // if this job is not duplicate
+				// count++
+				jobKey[temp.ID] = ""
+				allJobs = append(allJobs, temp)
+				fmt.Println(temp.ID + "," + temp.Title + "," + temp.Company + "," + temp.CompanyLocation + "," + temp.Salary)
+			} else {
+				// count++
+				fmt.Println("-------------" + temp.ID + " is duplicate -----------------")
+			}
 		})
 	})
 
 	// Before making a request print "Visiting ..."
 	collector.OnRequest(func(r *colly.Request) {
-		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36")
+		// r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36")
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	for page := 0; page < 2; page++ {
+	for page := 0; page < 1; page++ {
 		num := strconv.Itoa(page * 10)
 		link := fmt.Sprintf("https://uk.indeed.com/jobs?q=shop+assistant&l=London&start=%s", num)
 		collector.Visit(link)
@@ -89,8 +88,8 @@ func main() {
 	// collector3.Wait()
 
 	writeJSON(allJobs)
-
-	num := strconv.Itoa(count)
+	fmt.Println(count)
+	num := strconv.Itoa(len(allJobs))
 	fmt.Printf("Successfully crawled %s jobs", num)
 
 }
@@ -103,4 +102,13 @@ func writeJSON(data []job) {
 	}
 
 	_ = ioutil.WriteFile("jobs.json", file, 0644)
+}
+
+func removeS(str string) string {
+	if strings.ContainsAny(str, "+") {
+		result := strings.SplitAfter(str, "+")
+		return result[0]
+	} else {
+		return str
+	}
 }
