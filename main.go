6cbffd9badc826845640e03e5a7d74fc9b16a17d
Boris Chen <borischen0203@gmail.com>
2021-10-26 14:44:38 +0100

    UPD. update main file

--- main.go
+++ main.go
@@ -24,7 +24,6 @@ var (
 	jobKey  map[string]string
 	allJobs []job
 )
-var count = 0
 
 func main() {
 	fmt.Println("[Crawler start]")
@@ -51,33 +50,32 @@ func main() {
 	collector.OnHTML("#mosaic-provider-jobcards", func(e *colly.HTMLElement) {
 
 		e.ForEach(".tapItem", func(_ int, el *colly.HTMLElement) {
-			temp := job{
+			crawlJob := job{
 				ID:              el.Attr("data-jk"),
 				Title:           el.ChildAttr(".jobTitle-color-purple > span", "title"),
 				Company:         el.ChildText("span.companyName"),
-				CompanyLocation: removeS(el.ChildText("div.heading6.company_location.tapItem-gutter > pre > div")),
+				CompanyLocation: removeSuffix(el.ChildText("div.heading6.company_location.tapItem-gutter > pre > div")),
 				Salary:          el.ChildText(".salary-snippet-container .salary-snippet > span"),
 			}
 
 			//Use job id as key to check the crawler job is duplicate or not
-			if _, exist := jobKey[temp.ID]; !exist { // if this job is not duplicate
-				// count++
-				jobKey[temp.ID] = ""
-				allJobs = append(allJobs, temp)
-				fmt.Println(temp.ID + "," + temp.Title + "," + temp.Company + "," + temp.CompanyLocation + "," + temp.Salary)
+			if _, exist := jobKey[crawlJob.ID]; !exist { // if this job is not duplicate
+				jobKey[crawlJob.ID] = ""
+				allJobs = append(allJobs, crawlJob)
+				fmt.Printf("ID: %s |Title:%s |Company: %s |Location: %s |Salary: %s \n", crawlJob.ID, crawlJob.Title, crawlJob.Company, crawlJob.CompanyLocation, crawlJob.Salary)
 			} else {
-				// count++
-				fmt.Println("-------------" + temp.ID + " is duplicate -----------------")
+				fmt.Println("-------------" + crawlJob.ID + " is duplicate -----------------")
 			}
 		})
 	})
 
 	// Before making a request print "Visiting ..."
 	collector.OnRequest(func(r *colly.Request) {
-		// r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36")
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36")
 		fmt.Println("Visiting", r.URL.String())
 	})
 
+	//TODO: Check is this last page
 	for page := 0; page < 1; page++ {
 		num := strconv.Itoa(page * 10)
 		link := fmt.Sprintf("https://uk.indeed.com/jobs?q=shop+assistant&l=London&start=%s", num)
@@ -88,26 +86,24 @@ func main() {
 	// collector3.Wait()
 
 	writeJSON(allJobs)
-	fmt.Println(count)
-	num := strconv.Itoa(len(allJobs))
-	fmt.Printf("Successfully crawled %s jobs", num)
-
+	fmt.Printf("Successfully crawled %d jobs", len(allJobs))
 }
 
+//Output all jobs to json file
 func writeJSON(data []job) {
 	file, err := json.MarshalIndent(data, "", " ")
 	if err != nil {
 		log.Println("Unable to create json file")
 		return
 	}
-
 	_ = ioutil.WriteFile("jobs.json", file, 0644)
 }
 
-func removeS(str string) string {
+//Remvoe Company location Suffix word
+func removeSuffix(str string) string {
 	if strings.ContainsAny(str, "+") {
-		result := strings.SplitAfter(str, "+")
-		return result[0]
+		result := str[0:strings.IndexByte(str, '+')]
+		return result
 	} else {
 		return str
 	}
